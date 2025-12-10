5a7abff9842d351ce9e558caba15d37377a75b6f
westfallt13 <westfallt13@gmail.com>
2025-12-10 09:59:11 -0600

    Add missing preload.js file to fix AI functionality in desktop app

---
+++ preload.js
@@ -0,0 +1,30 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld('electronAPI', {
+    // Model management
+    loadModel: (modelPath, options) => ipcRenderer.invoke('load-model', modelPath, options),
+    unloadModel: () => ipcRenderer.invoke('unload-model'),
+    isModelLoaded: () => ipcRenderer.invoke('is-model-loaded'),
+    
+    // Inference
+    generateText: (prompt, options) => ipcRenderer.invoke('generate-text', prompt, options),
+    
+    // Progress updates
+    onGenerationProgress: (callback) => {
+        ipcRenderer.on('generation-progress', (event, data) => callback(data));
+    },
+    
+    // File operations
+    selectGgufFile: () => ipcRenderer.invoke('select-gguf-file'),
+    readGgufFile: (filePath) => ipcRenderer.invoke('read-gguf-file', filePath),
+    openModelsFolder: () => ipcRenderer.invoke('open-models-folder'),
+    scanModelsFolder: () => ipcRenderer.invoke('scan-models-folder'),
+    saveFile: (options) => ipcRenderer.invoke('save-file', options),
+    openFile: (filters) => ipcRenderer.invoke('open-file', filters),
+    
+    // App info
+    getAppVersion: () => ipcRenderer.invoke('get-app-version'),
+    getUserDataPath: () => ipcRenderer.invoke('get-user-data-path')
+});
